3904212a725dcbf52c2fd27c24287f2f8723a859
JeffBoss625 <mvoss9000@yahoo.com>
2020-05-26 18:26:41 -0400

    term.py example file

--- term.py
+++ term.py
@@ -1,15 +1,14 @@
 import curses as clib
 
+RED_BLACK = 1
 def test(scr):
-    keys = (
-        clib.KEY_BACKSPACE,
-    )
 
+    clib.init_pair(RED_BLACK, clib.COLOR_RED, clib.COLOR_BLACK)
     yoff = 0
-    for i in range (255, clib.KEY_BACKSPACE+55):
+    for i in range (255, clib.KEY_BACKSPACE):
         if clib.has_key(i):
             scr.move(yoff + 3, 3)
-            scr.addstr(clib.keyname(i).decode('utf-8') + ' [' + str(i) + ']')
+            scr.addstr(clib.keyname(i).decode('utf-8') + ' [' + str(i) + ']', clib.color_pair(RED_BLACK))
             yoff += 1
 
     scr.getch()
