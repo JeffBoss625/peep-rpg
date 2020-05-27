1c28deda2491eb1d9a0f1ffc0584ea534f0ad0a3
JeffBoss625 <mvoss9000@yahoo.com>
2020-05-26 20:10:22 -0400

    add color example

--- term.py
+++ term.py
@@ -1,16 +1,27 @@
-import curses as clib
+import curses as curses
 
 RED_BLACK = 1
 def test(scr):
 
-    clib.init_pair(RED_BLACK, clib.COLOR_RED, clib.COLOR_BLACK)
+    curses.init_pair(RED_BLACK, curses.COLOR_RED, curses.COLOR_BLACK)
     yoff = 0
-    for i in range (255, clib.KEY_BACKSPACE):
-        if clib.has_key(i):
-            scr.move(yoff + 3, 3)
-            scr.addstr(clib.keyname(i).decode('utf-8') + ' [' + str(i) + ']', clib.color_pair(RED_BLACK))
-            yoff += 1
+
+    curses.use_default_colors()
+    for i in range(0, 255):
+        curses.init_pair(i + 1, i, -1)
+    try:
+        for i in range(0, 255):
+            scr.addstr(str(i), curses.color_pair(i))
+
+    except:
+        pass
+
+    # for i in range (255, clib.KEY_BACKSPACE):
+    #     if clib.has_key(i):
+    #         scr.move(yoff + 3, 3)
+    #         scr.addstr(clib.keyname(i).decode('utf-8') + ' [' + str(i) + ']', clib.color_pair(RED_BLACK))
+    #         yoff += 1
 
     scr.getch()
 
-clib.wrapper(test)
+curses.wrapper(test)
