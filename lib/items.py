648d587c77dba259a00fba26328814549145311d
JeffBoss625 <mvoss9000@yahoo.com>
2020-09-27 12:15:48 -0400

    create new model modules: items, peeps

--- lib/items.py
+++ lib/items.py
@@ -4,18 +4,22 @@ from lib.constants import Color
 from lib.model import DataModel
 
 @dataclass
-class Ammo(DataModel):
+class Item(DataModel):
     def __post_init__(self):
         super().__init__()
-
     name: str = ''
     char: str = '?'
+    weight: int = 1
     fgcolor: str = Color.WHITE
     bgcolor: str = Color.BLACK
-    thaco: int = 20
+
+
+@dataclass
+class Ammo(Item):
     speed: int = 100
     ac: int = 20
     maxhp: int = 1
+    thaco: int = 20
 
     # temp state
     tics: int = 0
@@ -23,3 +27,8 @@ class Ammo(DataModel):
     attacks: dict = field(default_factory=dict)
     move_tactic: str = 'straight'
     direct: int = 0
+
+
+if __name__ == '__main__':
+    a = Ammo('dart', '/', Color.GREEN)
+    print(a)
