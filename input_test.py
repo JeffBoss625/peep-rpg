f964e719d0a17f3845fd7774fb9f5c1a34c42611
JeffBoss625 <mvoss9000@yahoo.com>
2021-02-15 22:49:04 -0500

    fix return value xy_offset()

--- input_test.py
+++ input_test.py
@@ -30,18 +30,52 @@ class SuspendCurses():
 def cp(i):
     return curses.color_pair(i)
 
+class COLOR:
+    BLACK   = 'black'
+    BLUE    = 'blue'
+    CYAN    = 'cyan'
+    GREEN   = 'green'
+    MAGENTA = 'magenta'
+    RED     = 'red'
+    WHITE   = 'white'
+    YELLOW  = 'yellow'
+    GRAY = 'gray'
+    LT_BLUE = 'lt_blue'
+    LT_GREEN = 'lt_green'
+    LT_AQUA = 'lt_aqua'
+    LT_RED = 'lt_red'
+    LT_PURPLE = 'lt_purple'
 
-def set_pairs(bg, ncolors):
-    for i, fg in enumerate(range(ncolors)):
-        curses.init_pair(i+1, fg, bg)
+COLOR8 = {
+    'black'   : curses.COLOR_BLACK,
+    'blue'    : curses.COLOR_BLUE,
+    'cyan'    : curses.COLOR_CYAN,
+    'green'   : curses.COLOR_GREEN,
+    'magenta' : curses.COLOR_MAGENTA,
+    'red'     : curses.COLOR_RED,
+    'white'   : curses.COLOR_WHITE,
+    'yellow'  : curses.COLOR_YELLOW,
+}
 
+
+def set_pairs(bg):
+    i = 1
+    for k in COLOR8.keys():
+        curses.init_pair(i, COLOR8[k], COLOR8[bg])
+        i += 1
+    for i in range(8, 16):
+        curses.init_pair(i + 1, i, COLOR8[bg])
 
 def main_loop(stdscr):
     try:
+        if not curses.can_change_color():
+            with SuspendCurses():
+                print(f'no color change support')
+            return 1
+
         curses.curs_set(1)  # set curses options and variables
         curses.noecho()
         curses.cbreak()
-        maxc = curses.COLORS
         maxy, maxx = stdscr.getmaxyx()
         h, w = 24, 80
         if maxy < h or maxx < w:
@@ -58,30 +92,22 @@ def main_loop(stdscr):
         test_win.bkgd(' ', cp(0))
         test_win.box()
         test_win.move(2, 2)
-        fgcol, bgcol = 0, 0
-        set_pairs(bgcol, maxc)
+        set_pairs(COLOR.BLACK)
         test_win.refresh()
+        # maxc = curses.COLORS
+        maxc = len(COLOR8)
         k, newk = 1, 2
         done = False
         while not done:
             if k > -1:
                 test_win.clear()
-                if k == ord('-'):  # decr currently selected attr
-                    fgcol -= 1
-                    if fgcol < 0: fgcol = maxc - 1
-                    set_pairs(fgcol, maxc)
-                if k == ord('+') or k == ord('='):  # incr currently selected attr
-                    fgcol += 1
-                    if fgcol > maxc - 1: fgcol = 0
-                    set_pairs(fgcol, maxc)
                 if k in (ord('q'), 27):
                     done = True
-                test_win.addstr(1, 10, f'{maxc} colors supported', cp(0))
-                test_win.addstr(2, 2, f'FG: {fgcol}  ', cp(0))
-                nrows = math.ceil(maxc/32)
-                for y in range(nrows):
-                    for x in range(32):
-                        test_win.addstr(4 + y, 3 + x*2, 'A ', cp(y*32 + x + 1))
+                test_win.addstr(1, 3, f'{len(COLOR8)} colors supported', cp(0))
+                for x in range(8):
+                    test_win.addstr(3, 2 + x * 2, 'A ', cp(x + 1))
+                for x in range(8):
+                    test_win.addstr(4, 2 + x * 2, 'A ', cp(x + 9))
                 test_win.move(1, 2)
                 test_win.box()
                 test_win.refresh()
