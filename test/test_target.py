0b1058605ff108f06cf68e5500bd410f73b83c78
JeffBoss625 <mvoss9000@yahoo.com>
2021-04-16 20:59:22 -0400

    fix tests

--- test/test_target.py
+++ test/test_target.py
@@ -6,7 +6,7 @@ from lib.startup import dummy_root
 from lib.target import *
 import lib.dungeons as dungeons
 from lib.peep_types import create_peep
-from lib.win_layout import Dim
+from lib.win_layout import Dim, Con
 
 
 def test_line_points():
@@ -68,28 +68,29 @@ def test_draw_target():
     random.seed = 1
     game = dungeons.create_game({
         'walls': [
-            '%%%%%%%%%%%%%%%%%%%%%%%%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%......................%',
-            '%%%%%%%%%%%%%%%%%%%%%%%%',
+            '%%%%%%%%%',
+            '%.......%',
+            '%.......%',
+            '%.......%',
+            '%.......%',
+            '%.......%',
+            '%.......%',
+            '%.......%',
+            '%%%%%%%%%',
         ],
         'peeps': [
-            create_peep('human', name='Super Dad', pos=(6,6)),
+            create_peep('human', name='Super Dad', pos=(4,4)),
         ]
     })
-    root_layout = dummy_root(dim=Dim(100, 22), logger=Logger('dbg.py'))
+    root_layout = dummy_root(dim=Dim(11, 11), logger=Logger('dbg.py'))
     control = PrpgControl(root_layout, game)
+    for comp in root_layout.info.comp_by_name.values():
+        if not comp.children and comp.name != 'maze':
+            comp.con = Con(0,0,1,1)
+    root_layout.do_layout()
     player = game.maze_model.peeps[0]
     for target in data:
         tp = (player.pos[0] + target[0], player.pos[1] + target[1])
         game.maze_model.target_path = tuple(line_points(player.pos, tp))
         control.root_layout.window.paint()
+        # control.root_layout.info.comp_by_name['maze'].window.paint()
