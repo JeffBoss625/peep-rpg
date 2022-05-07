30f60c8e446da243952e1a12b6cb30214e06d121
JeffBoss625 <mvoss9000@yahoo.com>
2022-05-07 16:22:40 -0400

    save node paths

--- examples/dijkstra.py
+++ examples/dijkstra.py
@@ -1,5 +1,5 @@
-from typing import List
-from typing import Dict
+from pprint import pprint
+from typing import List, Dict, Tuple, Any
 from dataclasses import dataclass
 
 
@@ -17,7 +17,7 @@ def add_edge(edges, id1, id2, len):
 @dataclass
 class Node:
     id: int
-    lengths: Dict[int, int]       # map of node ID to shortest length so far
+    paths: Dict[int, Tuple[List[Any], int]]  # PATHs and Total Lengths: map of node ID to tuple of (NODEs list and total length) 
     edges: List[Edge]
 
 def graph1(): 
@@ -66,30 +66,62 @@ def graph1():
 
     return nodes
 
-def update_lengths (nodes, origin_node_id, cur_node_id, edge, total_len):
-    total_len = total_len + edge.length
-    target_node_id = edge.node1
-    if target_node_id == cur_node_id: 
-        target_node_id = edge.node2
+def graph2(): 
+    nodes = {}      # nodes by id
 
-    target_node = nodes[target_node_id]
-    if origin_node_id in target_node.lengths and target_node.lengths[origin_node_id] < total_len:
+    # create nodes
+    for id in range(1, 4):
+        nodes[id] = Node(id, {}, [])
+
+    # create edges (by node1, node2 ids)
+    edges = {}
+    add_edge(edges, 1, 2, 5)
+    add_edge(edges,1, 3, 2)
+    add_edge(edges,2, 3, 1)
+    
+    # create edges/links
+    nodes[1].edges.append(edges[(1, 2)])
+    nodes[1].edges.append(edges[(1, 3)])
+
+    nodes[2].edges.append(edges[(2, 1)])
+    nodes[2].edges.append(edges[(2, 3)])
+
+    nodes[3].edges.append(edges[(3, 1)])
+    nodes[3].edges.append(edges[(3, 2)])
+
+    return nodes
+def update_lengths (all_nodes, cur_path, edge, total_len):
+    cur_node_id = cur_path[-1].id
+    origin_node_id = cur_path[0].id
+
+    target_node_id = edge.node1
+    if target_node_id == cur_node_id:
+        target_node_id = edge.node2
+    target_node = all_nodes[target_node_id]
+    
+    total_len = total_len + edge.length
+    print(f'update_lengths origin:{origin_node_id} current:{cur_node_id} targ:{target_node_id} total:{total_len}')
+    if origin_node_id in target_node.paths and target_node.paths[origin_node_id][1] < total_len:
         return  # better path already found
 
     # store best path and continue...
-    target_node.lengths[origin_node_id] = total_len
+    cur_path.append(target_node)
+    path_ids = tuple(n.id for n in cur_path)
+    target_node.paths[origin_node_id] = (path_ids, total_len)
     for e in target_node.edges:
-        update_lengths(nodes, origin_node_id, target_node.id, e, total_len)
+        if e.node1 not in (cur_node_id, origin_node_id) and e.node2 not in (cur_node_id, origin_node_id):
+            update_lengths(all_nodes, cur_path, e, total_len)
 
+    cur_path.pop()
 
-def dijkstra (nodes): 
-    for id in nodes:        # nodes by id
-        for e in nodes[id].edges:
-            update_lengths(nodes, id, id, e, 0)
+def dijkstra (all_nodes): 
+    for id in all_nodes:        # nodes by id
+        for e in all_nodes[id].edges:
+            update_lengths(all_nodes, [all_nodes[id]], e, 0)
             # update path n.id, edge
 
 if __name__ == '__main__':
-    g = graph1()
+    g = graph2()
     dijkstra(g)
-    print(g)
+    pprint(g)
         
