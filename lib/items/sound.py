ff426408d3132aedd8348bc3f9c0c04af22a6c15
JeffBoss625 <65090127+JeffBoss625@users.noreply.github.com>
2023-05-27 21:55:06 -0400

    Sleep function adjustments

--- lib/items/sound.py
+++ lib/items/sound.py
@@ -61,39 +61,69 @@ class PeepSleeping:
     timer: List[Any] = field(default_factory=list)
     target: float = 0.2
     cycles: int = 0
+    total_cycles: int = 0
+    cycle_target: int = 7
 
 def handle_sound(time, sleep):
     time_passed = time - sleep.age_checked
     sleep.age_checked = time
+    rem = sleep.limit
+    n3 = 0.8 * sleep.limit
+    n2 = 0.5 * sleep.limit
+    n1 = 0.2 * sleep.limit
+    if sleep.total_cycles >= sleep.cycle_target:
+        sleep.target = 0
+        sleep.sleepiness -= 0.025 * time_passed * ((sleep.cycles * 0.1) + 1)
+        if sleep.sleepiness <= sleep.target:
+            sleep.sleepiness = sleep.target
+            return time, sleep.sleepiness
+        return time, sleep.sleepiness
 
     if sleep.timer[0] is True and sleep.timer[1] <= 0:
-        if sleep.target == sleep.limit:
-            sleep.cycles += 1
-            if sleep.cycles > 5: sleep.cycles = 5
-            sleep.target = 0.2 * sleep.limit
+        if sleep.target == rem:
+            if sleep.cycles > 5:
+                sleep.total_cycles += 1
+            else:
+                sleep.cycles += 1
+                sleep.total_cycles += 1
+            sleep.target = n2
+            sleep.timer[0] = False
+        elif sleep.target == n3:
+            sleep.target = rem
+            sleep.timer[0] = False
+        elif sleep.target == n2:
+            sleep.target = n3
             sleep.timer[0] = False
         else:
-            sleep.target = sleep.limit
+            sleep.target = n2
             sleep.timer[0] = False
 
     if sleep.sleepiness < sleep.target:
-        sleep.sleepiness += 0.05 * time_passed * ((sleep.cycles * 0.1) + 1)
+        sleep.sleepiness += 0.025 * time_passed * ((sleep.cycles * 0.1) + 1)
         if sleep.sleepiness >= sleep.target:
             sleep.sleepiness = sleep.target
             sleep.timer[0] = True
-            if sleep.target == sleep.limit:
-                sleep.timer[1] = 20 * (1 - (sleep.cycles * 0.1))
+            if sleep.target == rem:
+                sleep.timer[1] = 5 * (1 + (sleep.cycles * 0.8))
+            elif sleep.target == n3:
+                sleep.timer[1] = 20 * (1 - (sleep.cycles * 0.15))
+            elif sleep.target == n2:
+                sleep.timer[1] = 25 * (1 + (sleep.cycles * 0.05))
             else:
-                sleep.timer[1] = 10 * (1 - (sleep.cycles * 0.1))
+                sleep.timer[1] = 5
     elif sleep.sleepiness > sleep.target:
-        sleep.sleepiness -= 0.05 * time_passed * ((sleep.cycles * 0.1) + 1)
+        sleep.sleepiness -= 0.025 * time_passed * ((sleep.cycles * 0.1) + 1)
         if sleep.sleepiness <= sleep.target:
             sleep.sleepiness = sleep.target
             sleep.timer[0] = True
-            if sleep.target == sleep.limit:
-                sleep.timer[1] = 10 * (1 - (sleep.cycles * 0.1))
+            if sleep.target == rem:
+                sleep.timer[1] = 5 * (1 + (sleep.cycles * 0.8))
+            elif sleep.target == n3:
+                sleep.timer[1] = 20 * (1 - (sleep.cycles * 0.15))
+            elif sleep.target == n2:
+                sleep.timer[1] = 25 * (1 + (sleep.cycles * 0.05))
             else:
-                sleep.timer[1] = 20 * (1 - (sleep.cycles * 0.1))
+                sleep.timer[1] = 5
     else:
         sleep.timer[1] -= time_passed
 
@@ -105,9 +135,9 @@ def handle_sound(time, sleep):
 if __name__ == '__main__':
     # import doctest
     # doctest.testmod()
-    sleep = PeepSleeping(0, 1, 0, [False, 0], 0.2, 0)
+    sleep = PeepSleeping(0, 1, 0, [False, 0], 0.2, 0, 0, 7)
     i = 0
-    while i <= 400:
+    while i <= 660:
         print(handle_sound(i, sleep))
-        i += 0.25
+        i += 0.5
 
